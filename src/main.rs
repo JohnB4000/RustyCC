1a660342861dbfe891d1652ff3a9c22fe1b3e0f5
John B <jb2069@hw.ac.uk>
2024-08-26 23:20:58 +0100

    Change to using clap for cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
 use std::{
-    env::args,
     fs::File,
     io::{BufReader, Read},
     process::exit,
 };
 
+use clap::{ArgGroup, Parser};
+
 #[allow(unused)]
 mod code_generator;
-//mod interpreter;
+mod interpreter;
 mod lexer;
 mod parser;
 
-fn main() {
-    let arguements: Vec<String> = args().collect();
-    let filename = match arguements.get(1) {
-        Some(filename) => filename,
-        None => {
-            println!("Arguement not provided: filename.");
-            exit(1)
-        }
-    };
+#[derive(Parser, Debug)]
+#[command(
+    name = "RustyCC",
+    version = "1.0",
+    author = "John Band",
+    about = "A Rust C Compiler"
+)]
+#[command(group(ArgGroup::new("mode").required(true).args(&["compiler_selected", "interpreter_selected"])))]
+struct Cli {
+    /// Use the RustyCC Compiler
+    #[arg(short = 'c', long = "compiler")]
+    compiler_selected: bool,
 
-    let file_handler = match File::open(filename) {
+    /// Use the RustyCC Interpreter
+    #[arg(short = 'i', long = "interpreter")]
+    interpreter_selected: bool,
+
+    /// File to be used
+    filename: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let file_handler = match File::open(&cli.filename) {
         Ok(file_handle) => file_handle,
         Err(error) => {
-            println!("Failed to open file {}, with error {:?}", filename, error);
+            println!(
+                "Failed to open file {}, with error {:?}",
+                cli.filename, error
+            );
             exit(1)
         }
     };
@@ -47,19 +65,23 @@ fn main() {
         }
     };
 
-    // match interpreter::interpret(ast) {
-    //     Ok(exit_code) => exit(exit_code),
-    //     Err(error) => {
-    //         println!("{:?}", error);
-    //         exit(1);
-    //     }
-    // }
-
-    match code_generator::generate_code(ast) {
-        Ok(()) => (),
-        Err(error) => {
-            println!("{:?}", error);
-            exit(1);
+    if cli.compiler_selected {
+        println!("Compiler has been selected");
+        match code_generator::generate_code(ast) {
+            Ok(()) => (),
+            Err(error) => {
+                println!("{:?}", error);
+                exit(1);
+            }
+        }
+    } else if cli.interpreter_selected {
+        println!("Interpreter has been selected");
+        match interpreter::interpret(ast) {
+            Ok(exit_code) => exit(exit_code),
+            Err(error) => {
+                println!("{:?}", error);
+                exit(1);
+            }
         }
     }
 }
